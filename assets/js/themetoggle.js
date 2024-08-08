f931b75b1588403e8f63ea94cda55b3db51882d4
donnacancode <donnacancode@gmail.com>
2024-08-08 15:41:12 -0400

    adjust toggle filename

--- assets/js/themetoggle.js
+++ assets/js/themetoggle.js
@@ -21,7 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
 
-    // Log the new theme
     console.log("Theme changed to:", theme);
   });
 });
